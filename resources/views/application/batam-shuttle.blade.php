9bb515862a8b24f73c1e8e87743ea583f9fc96a8
hjcodin <hj.codin@gmail.com>
2023-03-14 14:15:47 +0700

    menambahkan bagian harga pada halaman batam shuttle laundry

--- resources/views/application/batam-shuttle.blade.php
+++ resources/views/application/batam-shuttle.blade.php
@@ -40,18 +40,70 @@
             <h1 class="fw-bold">Batam Shuttle Laundry</h5>
                 <h3 class="fw-bold">Customers just stay in place, we will pick up your goods</h3>
         </div>
-        <div class="row mt-5">
-            <div class="col-md-3">
+        <div class="row ">
+            <div class="col-md-3 custom-col-batam-shuttle ">
                 <div class="card-body">
                     <i class="bi bi-bag-fill custom-icon-batam-shuttle"></i>
                     <h5 class="custom-text-batam-shuttle">
                         1. Online Order
                     </h5>
-                    <p class="card-text"></p>
+                    <p class="card-text">The customer arranges a schedule for picking up yout goods</p>
                 </div>
             </div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
+            <div class="col-md-3 custom-col-batam-shuttle">
+                <i class="bi bi-truck custom-icon-batam-shuttle"></i>
+                <h5 class="custom-text-batam-shuttle">
+                    2. We Pick Up
+                </h5>
+                <p class="card-text">
+                    Items that have been prepared by the customer will be picked up by us according to the schedule that has
+                    been arranged
+                </p>
+            </div>
+            <div class="col-md-3 custom-col-batam-shuttle">
+                <i class="bi bi-arrow-clockwise custom-icon-batam-shuttle"></i>
+                <h5 class="custom-text-batam-shuttle">
+                    Washing & Folding Process
+                </h5>
+                <p class="card-text">Your goods that have been taken by us, we will immediately wash and fold your goods and
+                    ready to be delivered back to your hands</p>
+            </div>
+            <div class="col-md-3 custom-col-batam-shuttle">
+                <i class=" bi bi-arrow-return-left custom-icon-batam-shuttle"></i>
+                <h5 class="custom-text-batam-shuttle">
+                    Delivered Back to Your Hands
+                </h5>
+                <p class="card-text">The results of your goods that we have washed, we will wrap them neatly back before
+                    being sent to your place.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 class="card-title custom-services-price-title">Price</h1>
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-3">
+                <h5 class="card-title">
+                    Dry Washing Only
+                </h5>
+                <p class="card-text">Rp4.000,00/kg</p>
+            </div>
+            <div class="col-md-3">
+                <h5 class="card-title">Wet Washing Only</h5>
+                <p class="card-text">Rp4.000,00.kg</p>
+            </div>
+            <div class="col-md-3">
+                <h5 class="card-title">Washing Only/Dry Washing Only/Wet Washing Only (Non Ironing)</h5>
+                <p class="card-text">Rp4.000,00/kg</p>
+            </div>
+            <div class="col-md-3">
+                <h5 class="card-title">
+                    Hanging Only [Non Dry Washing/Non Wet Washing/Non Ironing]
+                </h5>
+                <p class="card-text">Rp4.000,00/kg</p>
+
+            </div>
             <div class="col-md-3"></div>
         </div>
     </div>
