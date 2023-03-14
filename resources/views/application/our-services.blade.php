9754e8aa856f0c990e43b3e9789dea3cd5de1df0
hjcodin <hj.codin@gmail.com>
2023-03-14 07:53:45 +0700

    menyelesaikan halaman ourservices

--- resources/views/application/our-services.blade.php
+++ resources/views/application/our-services.blade.php
@@ -4,33 +4,40 @@
 @section('our-services')
     <div class="row custom-border background-services ">
         <div class="col-md-8">
-            <div class="col-md-6">
-                <div class="card-body ">
-                    <h5 class="card-title custom-services-text">
-                        The closest cheap laundry service
-                    </h5>
-                    <p class="card-text custom-services-p"> Nearby cheap laundry services serving Cemara Asri housing, we
-                        provide free shuttle services for the housing above, express/regular services, one day services
-                        aimed at the closest cheap kilo laundry to Cemara Asri housing, which more and more need fast and
-                        cheap,
-                    </p>
+            <div class="container-custom">
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title custom-services-text">
+                            The closest cheap laundry service
+                        </h5>
+                        <p class="card-text custom-services-p"> Nearby cheap laundry services serving Cemara Asri housing, we
+                            provide free shuttle services for the housing above, express/regular services, one day services
+                            aimed at the closest cheap kilo laundry to Cemara Asri housing, which more and more need fast
+                            and
+                            cheap,
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card-body">
-                    <h5 class="card-title custom-services-text">
-                        The closest cheap laundry service
-                    </h5>
-                    <p class="card-text custom-services-p"> Nearby cheap laundry services serving Cemara Asri housing, we
-                        provide free shuttle services for the housing above, express/regular services, one day services
-                        aimed at the closest cheap kilo laundry to Cemara Asri housing, which more and more need fast and
-                        cheap,
-                    </p>
-                    <a href="https://wa.me/6281372588889" class="btn-custom-services">+62 813-7258-8889</a>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title custom-services-text">
+                            The closest cheap laundry service
+                        </h5>
+                        <p class="card-text custom-services-p"> Nearby cheap laundry services serving Cemara Asri housing,
+                            we
+                            provide free shuttle services for the housing above, express/regular services, one day services
+                            aimed at the closest cheap kilo laundry to Cemara Asri housing, which more and more need fast
+                            and
+                            cheap,
+                        </p>
+                        <a href="https://wa.me/6281372588889" class="btn-custom-services"> TEL : +62
+                            813-7258-8889</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div class="row justify-content-center text-center mt-5">
         <div class="col-md-8">
             <div class="card-body">
@@ -84,40 +91,89 @@
             </div>
         </div>
         {{-- Down Section --}}
-        <div class="col-md-3">
-            <div class="card-body-process2">
-                <h5 class="card-title">
-                    Manual Wash
-                </h5>
-                <p class="card-text">We accept manual washing services for clothes that need special treatment, our team is
-                    very experienced so your clothes are guaranteed safe.</p>
+        <div class="row d-flex justify-content-center ">
+            <div class="col-md-3">
+                <div class="card-body-process2">
+                    <h5 class="card-title">
+                        Manual Wash
+                    </h5>
+                    <p class="card-text">We accept manual washing services for clothes that need special treatment, our team
+                        is
+                        very experienced so your clothes are guaranteed safe.</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body-process">
+                    <h5 class="card-title">
+                        Stain
+                    </h5>
+                    <p class="card-text">we provide stain removal services such as blood, ink, dirt, etc. using special
+                        tools to
+                        clean stains, our customers are very satisfied with the results so far, but we do not guarantee that
+                        all
+                        stains can be removed.</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body-process2">
+                    <h5 class="card-title">
+                        Just Iron
+                    </h5>
+                    <p class="card-text">Our cheap laundry services use special steam irons for laundry and experienced
+                        staff,
+                        so your clothes are safe from wrinkling and burning.</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body-process">
+                    <h5 class="card-title">
+                        Unit Laundry
+                    </h5>
+                    <p class="card-text">We provide special treatment for your expensive clothes with one washing machine
+                        for one customer or clothes, using a special laundry machine from the USA.</p>
+                </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card-body-process">
-                <h5 class="card-title">
-                    Stain
-                </h5>
-                <p class="card-text">we provide stain removal services such as blood, ink, dirt, etc. using special tools to
-                    clean stains, our customers are very satisfied with the results so far, but we do not guarantee that all
-                    stains can be removed.</p>
-            </div>
+
+
+    </div>
+    <div class="row justify-content-center mt-4 align-items-center text-center ">
+        <div class="col-md-8">
+            <h1 class="custom-text-card">The Nearest Cheap Laundry Kiloan
+                Money Back Guarantee</h1>
         </div>
-        <div class="col-md-3">
-            <div class="card-body-process2">
-                <h5 class="card-title">
-                    Just Iron
-                </h5>
-                <p class="card-text">Our cheap laundry services use special steam irons for laundry and experienced staff,
-                    so your clothes are safe from wrinkling and burning.</p>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <i class="fa-solid fa-arrow-right-arrow-left me-2 custom-icon mt-2"></i> <span class="fw-bold">
+                            Return of
+                            the
+                            same item
+                        </span>
+                    </h5>
+                    <p class="card-text  custom-p2 ">We will accept returns if the item <br> used by customer is damaged.
+                    </p>
+                </div>
+
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card-body-process">
+            <div class="col-md-4">
                 <h5 class="card-title">
-                    Unit Laundry
+                    <i class="fa-solid fa-repeat me-2 custom-icon mt-2"></i> <span class="fw-bold"> Repeated washing
+                    </span>
                 </h5>
-                <p class="card-text">We provide special treatment for your expensive clothes with one washing machine for one customer or clothes, using a special laundry machine from the USA.</p>
+                <p class="card-text custom-p2">We provide repeated washing services if the clothes are still dirty and have
+                    stains</p>
+            </div>
+            <div class="col-md-4">
+                <h5 class="card-title">
+                    <i class="fa-solid fa-money-bill-transfer mt-2 me-2 custom-icon"></i><span class="fw-bold"> Guarantee
+                        your
+                        money back
+                    </span>
+                </h5>
+                <p class="card-text custom-p2">We provide a money back guarantee if some of the customer's clothes are
+                    damaged</p>
             </div>
         </div>
     </div>
